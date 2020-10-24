cbc837b1978cef9da13963f45f4b6763d1c4d5c1
AlexandrRzz <rzz400@gmail.com>
2020-10-24 21:09:56 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App">
       <div className="wrapper">
         <header className="App-header">
+          <nav className="nav">
+            <ul className="nav__items">
+              <li className="nav__item nav__item-active">Day</li>
+              <li className="nav__item">Week</li>
+              <li className="nav__item">Month</li>
+              <li className="nav__item">Year</li>
+            </ul>
+          </nav>
         </header>
       </div>
     </div>
