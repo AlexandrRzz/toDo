526384532be624c7e7d981bd50518955576ec87a
AlexandrRzz <rzz400@gmail.com>
2020-12-02 21:30:13 +0200

    hide taskMenu when clicked outside of its field

--- src/components/TaskMenu/TaskMenu.js
+++ src/components/TaskMenu/TaskMenu.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import {useRef, useEffect} from 'react';
 import './TaskMenu.css';
 
 export default function TaskMenu({setContextMenu, togleTaskPin, deleteTask, task_id, pinned, memo}) {
+  
+  const wrapper = useRef(null);
+
+  function onClickOutside () {
+    setContextMenu({
+      toDoId: task_id,
+      showContext: false,
+      showMemo: false,
+    });
+  }
+
+  function handleDocumentClick(e) {
+    if (wrapper.current && !wrapper.current.contains(e.target)) {
+      onClickOutside();
+    }
+  };
+
+  function addOutsideClickListener() {
+    document.addEventListener('click', handleDocumentClick);
+  }
+
+  function removeOutsideClickListener() {
+    document.removeEventListener('click', handleDocumentClick);
+  }
+
+  function onShow () {
+    addOutsideClickListener();
+  }
+  
+  function onHide () {
+    removeOutsideClickListener();
+  }
+
+  useEffect(()=>{
+    onShow();
+    return () => {
+      onHide();
+    };
+  }, []);
+
   return (
-    <div className="menu">
+    <div 
+      className="menu"
+      ref={wrapper}
+    >
       <div 
         className="menu__item" 
         onClick={()=>{
@@ -12,6 +55,7 @@ export default function TaskMenu({setContextMenu, togleTaskPin, deleteTask, task
             showContext: false,
             showMemo: false,
           });
+          onHide();
           togleTaskPin(task_id);
         }}
       >
@@ -26,6 +70,7 @@ export default function TaskMenu({setContextMenu, togleTaskPin, deleteTask, task
             showContext: false,
             showMemo: true,
           });
+          onHide();
         }}
       >
         <i className="far fa-sticky-note menu__icon"></i>
@@ -39,6 +84,7 @@ export default function TaskMenu({setContextMenu, togleTaskPin, deleteTask, task
             showContext: false,
             showMemo: false,
           });
+          onHide();
           deleteTask(task_id);
         }}
       >
